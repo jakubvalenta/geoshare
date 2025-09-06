b85537574649c6bcf9f4f253686ba31b58c8267d
Jakub Valenta <jakub@jakubvalenta.cz>
2025-09-06 18:38:27 +0200

    ConversionMatcher: don't store the list reversed

--- app/src/main/java/page/ooooo/geoshare/lib/ConversionMatcher.kt
+++ app/src/main/java/page/ooooo/geoshare/lib/ConversionMatcher.kt
@@ -1,14 +1,14 @@
 package page.ooooo.geoshare.lib
 
+import androidx.compose.ui.util.fastForEachReversed
 import com.google.re2j.Matcher
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.roundToInt
 
-data class ConversionMatcher(val reversedMatchers: List<Matcher>) {
+data class ConversionMatcher(val matchers: List<Matcher>) {
     companion object {
-        fun fromConversionMatchers(conversionMatchers: List<ConversionMatcher>) = ConversionMatcher(
-            conversionMatchers.reversed().flatMap { it.reversedMatchers })
+        fun fromConversionMatchers(other: List<ConversionMatcher>) = ConversionMatcher(other.flatMap { it.matchers })
     }
 
     fun toPosition() = Position(
@@ -18,11 +18,14 @@ data class ConversionMatcher(val reversedMatchers: List<Matcher>) {
         z = getGroupOrNull("z")?.let { max(1, min(21, it.toDouble().roundToInt())).toString() },
     )
 
-    fun getGroupOrNull(name: String): String? = reversedMatchers.firstNotNullOfOrNull { m ->
-        try {
-            m.group(name)
-        } catch (_: IllegalArgumentException) {
-            null
+    fun getGroupOrNull(name: String): String? {
+        matchers.fastForEachReversed {
+            try {
+                return@getGroupOrNull it.group(name)
+            } catch (_: IllegalArgumentException) {
+                // Do nothing
+            }
         }
+        return null
     }
 }
