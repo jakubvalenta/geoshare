4a1c0a2c646baaf34d989593f26a9638437a11a9
Jakub Valenta <jakub@jakubvalenta.cz>
2025-09-09 15:44:59 +0200

    YandexMapsUrlConverter: update after converter generalisation

--- app/src/main/java/page/ooooo/geoshare/lib/converters/YandexMapsUrlConverter.kt
+++ app/src/main/java/page/ooooo/geoshare/lib/converters/YandexMapsUrlConverter.kt
@@ -3,83 +3,33 @@ package page.ooooo.geoshare.lib.converters
 import androidx.annotation.StringRes
 import com.google.re2j.Pattern
 import page.ooooo.geoshare.R
-import page.ooooo.geoshare.lib.*
-import java.net.URL
+import page.ooooo.geoshare.lib.allHtmlPattern
+import page.ooooo.geoshare.lib.allUrlPattern
 
-class YandexMapsUrlConverter(
-    private val log: ILog = DefaultLog(),
-    private val uriQuote: UriQuote = DefaultUriQuote(),
-) : UrlConverter {
+class YandexMapsUrlConverter() : UrlConverter {
     override val name = "Yandex Maps"
+
+    override val host: Pattern = Pattern.compile("""yandex.com""")
+    override val shortUrlPattern: Pattern = Pattern.compile("""https://yandex\.com/maps/-/.+""")
+
+    override val urlPattern = allUrlPattern {
+        optional {
+            query("z", z, sanitizeZoom)
+        }
+        first {
+            query("ll", "$lon,$lat")
+            path("""/maps/org/\d+/.*""")
+        }
+    }
+
+    override val htmlPattern = allHtmlPattern {
+        html(""".*?data-coordinates="$lon,$lat".*""")
+    }
+    override val htmlRedirectPattern = null
 
     @StringRes
     override val permissionTitleResId = R.string.converter_yandex_maps_permission_title
 
     @StringRes
     override val loadingIndicatorTitleResId = R.string.converter_yandex_maps_loading_indicator_title
-
-    val anyUrlPattern: Pattern = Pattern.compile("""^https?://yandex.com/maps.+$""")
-    val shortUrlPattern: Pattern = Pattern.compile("""^https?://yandex.com/maps/-/.+$""")
-
-    val latRegex = """\+?(?P<lat>-?\d{1,2}(\.\d{1,16})?)"""
-    val lonRegex = """\+?(?P<lon>-?\d{1,3}(\.\d{1,16})?)"""
-    val coordRegex = "$lonRegex,$latRegex"
-    val coordPattern: Pattern = Pattern.compile(coordRegex)
-    val zoomRegex = """(?P<z>\d{1,2}(\.\d{1,16})?)"""
-    val zoomPattern: Pattern = Pattern.compile(zoomRegex)
-
-    val urlPathPlacePattern: Pattern = Pattern.compile("""^/maps/org/\d+/.*$""")
-    val urlQueryPatterns = mapOf(
-        "ll" to coordPattern,
-    )
-    val htmlPattern: Pattern = Pattern.compile("""data-coordinates="$coordRegex"""")
-
-    override fun isSupportedUrl(url: URL): Boolean = anyUrlPattern.matcher(url.toString()).matches()
-
-    override fun isShortUrl(url: URL): Boolean = shortUrlPattern.matcher(url.toString()).matches()
-
-    override fun parseUrl(url: URL): ParseUrlResult? {
-        val position = Position()
-
-        // First add coordinates from query param '?ll=<lon>,<lat>'.
-        val urlQueryParams = getUrlQueryParams(url.query, uriQuote)
-        urlQueryPatterns.map { (paramName, paramPattern) ->
-            urlQueryParams[paramName]?.let { paramValue ->
-                paramPattern.matcher(paramValue).takeIf { m -> m.matches() }?.let { m ->
-                    position.addMatcher(m)
-                }
-            }
-        }
-
-        // THen add zoom from query param '?z=<z>'.
-        val zoomMatcher = urlQueryParams["z"]?.let { zoomPattern.matcher(it).takeIf { m -> m.matches() } }
-        zoomMatcher?.let { position.addMatcher(it) }
-
-        return if (position.lat != null && position.lon != null) {
-            log.i(null, "Yandex Maps URL converted $url > $position")
-            ParseUrlResult.Parsed(position)
-        } else {
-            // If this is a place URL '/maps/org/<id>', then parse coordinates from HTML.
-            val urlPath = uriQuote.decode(url.path)
-            if (urlPathPlacePattern.matches(urlPath)) {
-                log.i(null, "Yandex Maps URL requires HTML parsing to get coordinates for place id $url")
-                ParseUrlResult.RequiresHtmlParsing()
-            } else {
-                log.i(null, "Yandex Maps URL does not contain coordinates or place id $url")
-                null
-            }
-        }
-    }
-
-    override fun parseHtml(html: String): ParseHtmlResult? {
-        val m = htmlPattern.matcher(html).takeIf { it.find() }
-        if (m == null) {
-            log.w(null, "Yandex Maps HTML does not contain coordinates")
-            return null
-        }
-        val position = Position()
-        position.addMatcher(m)
-        log.i(null, "Yandex Maps HTML parsed $position")
-        return ParseHtmlResult.Parsed(position)
-    }
 }
--- app/src/test/java/page/ooooo/geoshare/YandexMapsUrlConverterTest.kt
+++ app/src/test/java/page/ooooo/geoshare/YandexMapsUrlConverterTest.kt
@@ -3,110 +3,105 @@ package page.ooooo.geoshare
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import page.ooooo.geoshare.lib.FakeLog
-import page.ooooo.geoshare.lib.FakeUriQuote
 import page.ooooo.geoshare.lib.Position
-import page.ooooo.geoshare.lib.converters.ParseHtmlResult
-import page.ooooo.geoshare.lib.converters.ParseUrlResult
 import page.ooooo.geoshare.lib.converters.YandexMapsUrlConverter
 import java.net.URL
 
-class YandexMapsUrlConverterTest {
-    private lateinit var yandexMapsUrlConverter: YandexMapsUrlConverter
-
+class YandexMapsUrlConverterTest : BaseUrlConverterTest() {
     @Before
-    fun before() {
-        yandexMapsUrlConverter = YandexMapsUrlConverter(FakeLog(), FakeUriQuote())
+    fun before2() {
+        urlConverter = YandexMapsUrlConverter()
     }
 
     @Test
     fun isSupportedUrl_unknownProtocol() {
-        assertFalse(yandexMapsUrlConverter.isSupportedUrl(URL("ftp://yandex.com/maps?whatshere%5Bpoint%5D=144.96315783657045%2C-37.81384550131279&whatshere%5Bzoom%5D=17.852003&ll=144.96315783657042%2C-37.81384550094835&z=17.852003&si=6u8menx2bg23cfx27y7p1je8y8")))
+        assertTrue(isSupportedUrl(URL("ftp://yandex.com/maps?whatshere%5Bpoint%5D=144.96315783657045%2C-37.81384550131279&whatshere%5Bzoom%5D=17.852003&ll=144.96315783657042%2C-37.81384550094835&z=17.852003&si=6u8menx2bg23cfx27y7p1je8y8")))
     }
 
     @Test
     fun isSupportedUrl_unknownHost() {
-        assertFalse(yandexMapsUrlConverter.isSupportedUrl(URL("https://www.example.com/")))
+        assertFalse(isSupportedUrl(URL("https://www.example.com/")))
     }
 
     @Test
     fun isSupportedUrl_supportedUrl() {
-        assertTrue(yandexMapsUrlConverter.isSupportedUrl(URL("https://yandex.com/maps?whatshere%5Bpoint%5D=144.96315783657045%2C-37.81384550131279&whatshere%5Bzoom%5D=17.852003&ll=144.96315783657042%2C-37.81384550094835&z=17.852003&si=6u8menx2bg23cfx27y7p1je8y8")))
+        assertTrue(isSupportedUrl(URL("https://yandex.com/maps?whatshere%5Bpoint%5D=144.96315783657045%2C-37.81384550131279&whatshere%5Bzoom%5D=17.852003&ll=144.96315783657042%2C-37.81384550094835&z=17.852003&si=6u8menx2bg23cfx27y7p1je8y8")))
     }
 
     @Test
     fun isSupportedUrl_shortUrl() {
-        assertTrue(yandexMapsUrlConverter.isSupportedUrl(URL("https://yandex.com/maps/-/CLAvMI18")))
+        assertTrue(isSupportedUrl(URL("https://yandex.com/maps/-/CLAvMI18")))
     }
 
     @Test
     fun parseUrl_noPathOrKnownUrlQueryParams() {
-        assertNull(yandexMapsUrlConverter.parseUrl(URL("https://yandex.com")))
-        assertNull(yandexMapsUrlConverter.parseUrl(URL("https://yandex.com/")))
-        assertNull(yandexMapsUrlConverter.parseUrl(URL("https://yandex.com/maps")))
-        assertNull(yandexMapsUrlConverter.parseUrl(URL("https://yandex.com/maps/?spam=1")))
+        assertNull(parseUrl(URL("https://yandex.com")))
+        assertNull(parseUrl(URL("https://yandex.com/")))
+        assertNull(parseUrl(URL("https://yandex.com/maps")))
+        assertNull(parseUrl(URL("https://yandex.com/maps/?spam=1")))
     }
 
     @Test
     fun parseUrl_coordinates() {
         assertEquals(
-            ParseUrlResult.Parsed(Position("-37.81384550094835", "144.96315783657042", z = "18")),
-            yandexMapsUrlConverter.parseUrl(URL("https://yandex.com/maps?whatshere%5Bpoint%5D=144.96315783657045%2C-37.81384550131279&whatshere%5Bzoom%5D=17.852003&ll=144.96315783657042%2C-37.81384550094835&z=17.852003&si=6u8menx2bg23cfx27y7p1je8y8"))
+            Position("-37.81384550094835", "144.96315783657042", z = "18"),
+            parseUrl(URL("https://yandex.com/maps?whatshere%5Bpoint%5D=144.96315783657045%2C-37.81384550131279&whatshere%5Bzoom%5D=17.852003&ll=144.96315783657042%2C-37.81384550094835&z=17.852003&si=6u8menx2bg23cfx27y7p1je8y8"))
         )
     }
 
     @Test
     fun parseUrl_placeAndCoordinates() {
         assertEquals(
-            ParseUrlResult.Parsed(Position("50.111192", "8.668963", z = "14")),
-            yandexMapsUrlConverter.parseUrl(URL("https://yandex.com/maps/org/94933420809/?display-text=Cafes&ll=8.668963%2C50.111192&mode=search&sctx=ZAAAAAgBEAAaKAoSCTHO34RCVCFAETJyFva0DUlAEhIJRii2gqYldj8R51JcVfZdYT8iBgABAgMEBSgKOABAZEgBYkZyZWFycj1zY2hlbWVfTG9jYWwvR2VvdXBwZXIvQWR2ZXJ0cy9SZWFycmFuZ2VCeUF1Y3Rpb24vQ2FjaGUvRW5hYmxlZD0xagJkZZ0BzczMPaABAKgBAL0ByteiIsIBkAGZj5fsswa4y%2FDcfpayrLSaAYmW5NPhAure6aC4Abn7yYWlA%2F3d2IRjiqSy14AG5PuvhaAE%2BoyK7rEC5Pu%2F75oF7L%2FyxdIDnOOpmucBt6iSh6UCyK%2FLuGyX48CmjwWFwNHQXv7d0vblBLXx6pSFA5y6x%2BXwBYy0i4Jx4oP6l8QG%2FevBrP0FnZn7uHOCpuWC9AaCAgjQmtCw0YTQtYoCNjE4NDEwNjM5MCQzNTE5MzExNDkzNyQxODQxMDYzOTQkMTg0MTA2MzkyJDIyMzA1MDc4MDc4NJICAJoCDGRlc2t0b3AtbWFwcw%3D%3D&sll=8.674635%2C50.129382&sspn=0.076143%2C0.041160&text=%D0%9A%D0%B0%D1%84%D0%B5&z=14.19"))
+            Position("50.111192", "8.668963", z = "14"),
+            parseUrl(URL("https://yandex.com/maps/org/94933420809/?display-text=Cafes&ll=8.668963%2C50.111192&mode=search&sctx=ZAAAAAgBEAAaKAoSCTHO34RCVCFAETJyFva0DUlAEhIJRii2gqYldj8R51JcVfZdYT8iBgABAgMEBSgKOABAZEgBYkZyZWFycj1zY2hlbWVfTG9jYWwvR2VvdXBwZXIvQWR2ZXJ0cy9SZWFycmFuZ2VCeUF1Y3Rpb24vQ2FjaGUvRW5hYmxlZD0xagJkZZ0BzczMPaABAKgBAL0ByteiIsIBkAGZj5fsswa4y%2FDcfpayrLSaAYmW5NPhAure6aC4Abn7yYWlA%2F3d2IRjiqSy14AG5PuvhaAE%2BoyK7rEC5Pu%2F75oF7L%2FyxdIDnOOpmucBt6iSh6UCyK%2FLuGyX48CmjwWFwNHQXv7d0vblBLXx6pSFA5y6x%2BXwBYy0i4Jx4oP6l8QG%2FevBrP0FnZn7uHOCpuWC9AaCAgjQmtCw0YTQtYoCNjE4NDEwNjM5MCQzNTE5MzExNDkzNyQxODQxMDYzOTQkMTg0MTA2MzkyJDIyMzA1MDc4MDc4NJICAJoCDGRlc2t0b3AtbWFwcw%3D%3D&sll=8.674635%2C50.129382&sspn=0.076143%2C0.041160&text=%D0%9A%D0%B0%D1%84%D0%B5&z=14.19"))
         )
     }
 
     @Test
     fun parseUrl_placeOnly() {
-        assertTrue(
-            yandexMapsUrlConverter.parseUrl(URL("https://yandex.com/maps/org/94933420809/")) is ParseUrlResult.RequiresHtmlParsing
+        assertEquals(
+            Position(),
+            parseUrl(URL("https://yandex.com/maps/org/94933420809/"))
         )
     }
 
     @Test
     fun parseHtml_containsDataCoordinates_returnsPosition() {
         assertEquals(
-            ParseHtmlResult.Parsed(Position("50.106376", "8.664164")),
-            yandexMapsUrlConverter.parseHtml("""<html><div data-coordinates="8.664164,50.106376"></div></html>""")
+            Position("50.106376", "8.664164"),
+            parseHtml("""<html><div data-coordinates="8.664164,50.106376"></div></html>""")
         )
     }
 
     @Test
     fun parseHtml_containsInvalidDataCoordinates_returnsNull() {
         assertNull(
-            yandexMapsUrlConverter.parseHtml("""<html><div data-coordinates="spam"></div></html>""")
+            parseHtml("""<html><div data-coordinates="spam"></div></html>""")
         )
     }
 
     @Test
     fun parseHtml_doesNotContainCoordinates_returnsNull() {
         assertNull(
-            yandexMapsUrlConverter.parseHtml("""<html></html>""")
+            parseHtml("""<html></html>""")
         )
     }
 
     @Test
     fun isShortUrl_correct() {
-        assertTrue(yandexMapsUrlConverter.isShortUrl(URL("https://yandex.com/maps/-/CLAvMI18")))
+        assertTrue(isShortUrl(URL("https://yandex.com/maps/-/CLAvMI18")))
     }
 
     @Test
     fun isShortUri_wrongPath() {
-        assertFalse(yandexMapsUrlConverter.isShortUrl(URL("https://yandex.com/")))
-        assertFalse(yandexMapsUrlConverter.isShortUrl(URL("https://yandex.com/maps/")))
-        assertFalse(yandexMapsUrlConverter.isShortUrl(URL("https://yandex.com/maps/-/")))
-        assertFalse(yandexMapsUrlConverter.isShortUrl(URL("https://yandex.com/foo")))
+        assertFalse(isShortUrl(URL("https://yandex.com/")))
+        assertFalse(isShortUrl(URL("https://yandex.com/maps/")))
+        assertFalse(isShortUrl(URL("https://yandex.com/maps/-/")))
+        assertFalse(isShortUrl(URL("https://yandex.com/foo")))
     }
 
     @Test
     fun isShortUri_unknownDomain() {
-        assertFalse(yandexMapsUrlConverter.isShortUrl(URL("https://www.example.com/foo")))
+        assertFalse(isShortUrl(URL("https://www.example.com/foo")))
     }
 }
