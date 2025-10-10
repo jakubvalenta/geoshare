340791d09815d91d6abbef6ebe110c81bb8b9ec4
Jakub Valenta <jakub@jakubvalenta.cz>
2025-10-11 00:10:43 +0200

    ResultAutomationRow: fix text overflow when animating

--- app/src/main/java/page/ooooo/geoshare/ui/components/ResultAutomationRow.kt
+++ app/src/main/java/page/ooooo/geoshare/ui/components/ResultAutomationRow.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
@@ -128,6 +129,8 @@ private fun RowScope.ResultAutomationNotification(
                 Text(
                     text(),
                     style = MaterialTheme.typography.labelLarge,
+                    maxLines = 1,
+                    overflow = TextOverflow.MiddleEllipsis,
                 )
             }
         }
