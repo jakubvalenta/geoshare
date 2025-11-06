829ba1a31baec6fc1db3b1f8c9266325595e045b
Jakub Valenta <jakub@jakubvalenta.cz>
2025-11-06 18:17:02 +0100

    DoubleExtensions: don't use scientific notation

--- app/src/main/java/page/ooooo/geoshare/lib/DoubleExtensions.kt
+++ app/src/main/java/page/ooooo/geoshare/lib/DoubleExtensions.kt
@@ -5,5 +5,5 @@ import kotlin.math.roundToInt
 fun Double.toTrimmedString(): String = if (this % 1.0 == 0.0) {
     this.roundToInt().toString()
 } else {
-    this.toString().trimEnd('0').trimEnd('.')
+    String.format("%.15f", this).trimEnd('0').trimEnd('.')
 }
