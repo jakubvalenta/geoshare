96b719141687a8696a7b2a0d0967a6029b48ef82
Jakub Valenta <jakub@jakubvalenta.cz>
2026-01-23 17:15:33 +0100

    CoordinatesOutputTest: fix

--- app/src/test/java/page/ooooo/geoshare/lib/outputs/CoordinatesOutputTest.kt
+++ app/src/test/java/page/ooooo/geoshare/lib/outputs/CoordinatesOutputTest.kt
@@ -4,10 +4,10 @@ import kotlinx.collections.immutable.persistentListOf
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import page.ooooo.geoshare.lib.FakeUriQuote
+import page.ooooo.geoshare.lib.UriQuote
 import page.ooooo.geoshare.lib.position.Point
 import page.ooooo.geoshare.lib.position.Position
 import page.ooooo.geoshare.lib.position.Srs
-import page.ooooo.geoshare.lib.UriQuote
 
 class CoordinatesOutputTest {
     private var uriQuote: UriQuote = FakeUriQuote()
@@ -35,54 +35,78 @@ class CoordinatesOutputTest {
     @Test
     fun copyAction_returnsSouthWestForNegativeCoordinates() {
         assertEquals(
-            "17°\u00a012′\u00a059.65956″\u00a0S, 149°\u00a056′\u00a049.30584″\u00a0W",
-            output.getPositionActions().firstNotNullOf { it as? CopyAction }
-                .getText(Position(Srs.WGS84, -17.2165721, -149.9470294), null, uriQuote),
+            listOf(
+                "-17.2165721, -149.9470294",
+                "17°\u00a012′\u00a059.65956″\u00a0S, 149°\u00a056′\u00a049.30584″\u00a0W",
+            ),
+            Position(Srs.WGS84, -17.2165721, -149.9470294).let { position ->
+                output.getPositionActions().map { (it as? CopyAction)?.getText(position, null, uriQuote) }
+            },
         )
     }
 
     @Test
     fun copyAction_returnsNorthEastForPositiveCoordinates() {
         assertEquals(
-            "52°\u00a030′\u00a024.22656″\u00a0N, 13°\u00a015′\u00a035.75124″\u00a0E",
-            output.getPositionActions().firstNotNullOf { it as? CopyAction }
-                .getText(Position(Srs.WGS84, 52.5067296, 13.2599309), null, uriQuote),
+            listOf(
+                "52.5067296, 13.2599309",
+                "52°\u00a030′\u00a024.22656″\u00a0N, 13°\u00a015′\u00a035.75124″\u00a0E",
+            ),
+            Position(Srs.WGS84, 52.5067296, 13.2599309).let { position ->
+                output.getPositionActions().map { (it as? CopyAction)?.getText(position, null, uriQuote) }
+            },
         )
     }
 
     @Test
     fun copyAction_returnsZerosForZeroCoordinates() {
         assertEquals(
-            "0°\u00a00′\u00a00.0″\u00a0N, 0°\u00a00′\u00a00.0″\u00a0E",
-            output.getPositionActions().firstNotNullOf { it as? CopyAction }
-                .getText(Position(Srs.WGS84, 0.0, 0.0), null, uriQuote),
+            listOf(
+                "0, 0",
+                "0°\u00a00′\u00a00.0″\u00a0N, 0°\u00a00′\u00a00.0″\u00a0E",
+            ),
+            Position(Srs.WGS84, 0.0, 0.0).let { position ->
+                output.getPositionActions().map { (it as? CopyAction)?.getText(position, null, uriQuote) }
+            },
         )
     }
 
     @Test
     fun copyAction_returnsZeroDegForZeroDegCoordinates() {
         assertEquals(
-            "0°\u00a030′\u00a00.0″\u00a0N, 0°\u00a030′\u00a00.0″\u00a0E",
-            output.getPositionActions().firstNotNullOf { it as? CopyAction }
-                .getText(Position(Srs.WGS84, 0.5, 0.5), null, uriQuote),
+            listOf(
+                "0.5, 0.5",
+                "0°\u00a030′\u00a00.0″\u00a0N, 0°\u00a030′\u00a00.0″\u00a0E",
+            ),
+            Position(Srs.WGS84, 0.5, 0.5).let { position ->
+                output.getPositionActions().map { (it as? CopyAction)?.getText(position, null, uriQuote) }
+            },
         )
     }
 
     @Test
     fun copyAction_returnsZeroMinForZeroMinCoordinates() {
         assertEquals(
-            "10°\u00a00′\u00a00.0″\u00a0S, 20°\u00a00′\u00a00.0″\u00a0W",
-            output.getPositionActions().firstNotNullOf { it as? CopyAction }
-                .getText(Position(Srs.WGS84, -10.0, -20.0), null, uriQuote)
+            listOf(
+                "-10, -20",
+                "10°\u00a00′\u00a00.0″\u00a0S, 20°\u00a00′\u00a00.0″\u00a0W",
+            ),
+            Position(Srs.WGS84, -10.0, -20.0).let { position ->
+                output.getPositionActions().map { (it as? CopyAction)?.getText(position, null, uriQuote) }
+            },
         )
     }
 
     @Test
     fun copyAction_returnsZerosSecForZeroSecCoordinates() {
         assertEquals(
-            "10°\u00a030′\u00a00.0″\u00a0S, 20°\u00a030′\u00a00.0″\u00a0W",
-            output.getPositionActions().firstNotNullOf { it as? CopyAction }
-                .getText(Position(Srs.WGS84, -10.5, -20.5), null, uriQuote)
+            listOf(
+                "-10.5, -20.5",
+                "10°\u00a030′\u00a00.0″\u00a0S, 20°\u00a030′\u00a00.0″\u00a0W",
+            ),
+            Position(Srs.WGS84, -10.5, -20.5).let { position ->
+                output.getPositionActions().map { (it as? CopyAction)?.getText(position, null, uriQuote) }
+            },
         )
     }
 
