673bd075d07d01c436a0e5f7d3cd40681eaee7bb
Jakub Valenta <jakub@jakubvalenta.cz>
2026-02-09 08:30:55 +0100

    v5.15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 25
         // noinspection EditedTargetSdkVersion
         targetSdk = 36
-        versionCode = 33
-        versionName = "5.14.0"
+        versionCode = 34
+        versionName = "5.15.0"
 
         androidResources {
             @Suppress("UnstableApiUsage")
