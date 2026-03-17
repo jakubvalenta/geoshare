b09c7c8e4c8554f83f40d092685165623fdc5512
Jakub Valenta <jakub@jakubvalenta.cz>
2026-03-17 19:07:31 +0100

    AmapInputBehaviorTest: update

--- app/src/androidTest/java/page/ooooo/geoshare/inputs/AmapInputBehaviorTest.kt
+++ app/src/androidTest/java/page/ooooo/geoshare/inputs/AmapInputBehaviorTest.kt
@@ -11,7 +11,10 @@ class AmapInputBehaviorTest : BaseInputBehaviorTest() {
 
         // Short URI
         testUri(
-            GCJ02Point(31.222811749011463, 121.46840706467624),
+            GCJ02Point(
+                31.222811749011463, 121.46840706467624,
+                name = "上海市黄浦区巨鹿路15-17号",
+            ),
             "https://surl.amap.com/4mkKGuyJ2bz",
         )
     }
