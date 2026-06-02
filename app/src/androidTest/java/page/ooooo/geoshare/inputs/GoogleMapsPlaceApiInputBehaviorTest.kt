cad001bc5606765ef52e7be04ef7c2816d3c6958
Jakub Valenta <jakub@jakubvalenta.cz>
2026-06-02 23:21:35 +0200

    GoogleMapsPlaceApiInputBehaviorTest: fix on physical dev

--- app/src/androidTest/java/page/ooooo/geoshare/inputs/GoogleMapsPlaceApiInputBehaviorTest.kt
+++ app/src/androidTest/java/page/ooooo/geoshare/inputs/GoogleMapsPlaceApiInputBehaviorTest.kt
@@ -88,7 +88,7 @@ class GoogleMapsPlaceApiInputBehaviorTest(private val testServerParams: TestServ
             fallbackPoint = if (testServer is TestServer.Configured) {
                 null
             } else if (htmlParsingSupported) {
-                WGS84Point(47.5951518, -122.3316394, name = "Lumen Field", source = Source.API)
+                WGS84Point(47.5951518, -122.3316394, name = "Lumen Field", source = Source.URI)
             } else {
                 null
             },
