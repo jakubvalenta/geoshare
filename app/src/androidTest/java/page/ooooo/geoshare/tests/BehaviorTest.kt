cd2003cbca1d848ca57427f4de8c5fc57e35b523
Jakub Valenta <jakub@jakubvalenta.cz>
2026-09-11 22:10:42 +0200

    UserPreferencesBehaviorTest: fix on Redmi 8

--- app/src/androidTest/java/page/ooooo/geoshare/tests/BehaviorTest.kt
+++ app/src/androidTest/java/page/ooooo/geoshare/tests/BehaviorTest.kt
@@ -473,7 +473,7 @@ fun UiAutomatorTestScope.scrollToLinkIcons() {
             try {
                 scroll(Direction.DOWN, 2f)
             } catch (_: StaleObjectException) {
-                // Stale object can happen on Xiaomi, do nothing
+                // Stale object can happen on Redmi 8, do nothing
             }
         }
     }
